066bb91f7945f07c4f7c0bfdba73cade5a475600
Phue Pwint San <137300246+phuepwintsan@users.noreply.github.com>
2026-01-11 15:04:58 +0630

    Update naming

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -1,5 +1,5 @@
-import NavBar from "../components/NavBar";
-import Footer from "../components/Footer";
+import NavBar from "../components/NavBars";
+import Footer from "../components/Footers";
 import AboutUs from "../components/AboutUs";
 
 export default function AboutUsPage() {
