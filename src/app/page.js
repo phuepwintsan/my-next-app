b90eda1aecf4264a3ab26d9d6c6e50d5bfbae88d
Phue Pwint San <137300246+phuepwintsan@users.noreply.github.com>
2026-01-11 15:03:38 +0630

    Update name

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import NavBar from "./components/NavBar";
-import HeroBanner from "./components/Herobanner";
+import NavBar from "./components/NavBars";
+import HeroBanner from "./components/HeroBanners";
 import Explore from "./components/Explore";
 import Chosen from "./components/Chosen";
 import Review from "./components/Reviews";
-import Footer from "./components/Footer";
+import Footer from "./components/Footers";
 
 export default function Home() {
   return (
